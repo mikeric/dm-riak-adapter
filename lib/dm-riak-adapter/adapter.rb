6bf7ad7a834525098f2ac8675dd84d332f1f54a2
Mike Richards <mike22e@gmail.com>
2010-04-06 00:57:24 -0400

    Added YARD documentation to RiakAdapter

--- lib/dm-riak-adapter/adapter.rb
+++ lib/dm-riak-adapter/adapter.rb
@@ -1,5 +1,17 @@
 module DataMapper::Adapters
   class RiakAdapter < AbstractAdapter
+    # Initializes a new RiakAdapter instance
+    # 
+    # @param [String, Symbol] name
+    #   Repository name
+    # 
+    # @param [Hash] options
+    #   Configuration options
+    # 
+    # @option options [String] :host ('127.0.0.1') Server hostname
+    # @option options [Integer] :port (8098) Server port
+    # @option options [String] :prefix ('riak') Path prefix to the HTTP endpoint
+    # @option options [String] :namespace ('') Bucket namespace
     def initialize(name, options)
       super
       
@@ -11,10 +23,30 @@ module DataMapper::Adapters
       @namespace = options[:namespace] ? options[:namespace] + ':' : ''
     end
     
+    # Persists one or many new resources
+    # 
+    # @example
+    #   adapter.create(collection)  # => 1
+    # 
+    # @param [Enumerable<Resource>] resources
+    #   List of resources (model instances) to create
+    # 
+    # @return [Integer]
+    #   Number of objects created
     def create(resources)
       create_objects(resources)
     end
     
+    # Reads one or many resources from a datastore
+    # 
+    # @example
+    #   adapter.read(query)  # => [{'title' => 'Lorem Ipsum'}]
+    # 
+    # @param [Query] query
+    #   Query to match objects in the datastore
+    # 
+    # @return [Enumerable<Hash>]
+    #   Array of hashes to become resources
     def read(query)
       query.filter_records(objects_for(query.model)).each do |object|
         query.fields.each do |property|
@@ -23,6 +55,19 @@ module DataMapper::Adapters
       end
     end
     
+    # Updates one or many existing resources
+    # 
+    # @example
+    #   adapter.update(attributes, collection)  # => 1
+    # 
+    # @param [Hash(Property => Object)] attributes
+    #   Hash of attribute values to set, keyed by Property
+    # 
+    # @param [Collection] collection
+    #   Collection of records to be updated
+    # 
+    # @return [Integer]
+    #   Number of records updated
     def update(attributes, collection)
       attributes = attributes_as_fields(attributes)
       
@@ -30,10 +75,30 @@ module DataMapper::Adapters
       update_objects(collection)
     end
     
+    # Deletes one or many existing resources
+    # 
+    # @example
+    #   adapter.delete(collection)  # => 1
+    # 
+    # @param [Collection] collection
+    #   Collection of records to be deleted
+    # 
+    # @return [Integer]
+    #   Number of records deleted
     def delete(collection)
       delete_objects(collection)
     end
     
+    # Flushes the bucket for the specified model
+    # 
+    # @example
+    #   adapter.flush(Post)  # => ["6moGsRVfutpG4wzibgwDBKc37Dd"]
+    # 
+    # @param [Class] model
+    #   Model to flush
+    # 
+    # @return [Array<String>]
+    #   Keys of the flushed objects
     def flush(model)
       bucket(model).keys.each {|key| bucket(model)[key].delete}
     end
